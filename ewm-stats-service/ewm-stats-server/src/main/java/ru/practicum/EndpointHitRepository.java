9d054f2a805860ae2fca57369ed460e8d8c680a4
Kamil Medjidov <captain-kam@yandex.ru>
2026-04-09 16:42:14 +0400

    Добавил сортировку в методы вывода статистики

--- ewm-stats-service/ewm-stats-server/src/main/java/ru/practicum/EndpointHitRepository.java
+++ ewm-stats-service/ewm-stats-server/src/main/java/ru/practicum/EndpointHitRepository.java
@@ -15,16 +15,18 @@ public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> 
                WHERE   e.timestamp BETWEEN :periodStart AND :periodEnd
                  AND   (:uris IS NULL OR e.uri IN :uris)
             GROUP BY   e.app, e.uri
+            ORDER BY   COUNT(e.id) DESC
             """)
     List<ViewStats> findStats(LocalDateTime periodStart, LocalDateTime periodEnd, List<String> uris);
 
 
     @Query("""
-              SELECT   new ru.practicum.ViewStats(e.app, e.uri, COUNT(DISTINCT e.id))
+              SELECT   new ru.practicum.ViewStats(e.app, e.uri, COUNT(DISTINCT e.ip))
                 FROM   EndpointHit e
                WHERE   e.timestamp BETWEEN :periodStart AND :periodEnd
                  AND   (:uris IS NULL OR e.uri IN :uris)
             GROUP BY   e.app, e.uri
+            ORDER BY   COUNT(DISTINCT e.ip) DESC
             """)
     List<ViewStats> findUniqueStats(LocalDateTime periodStart, LocalDateTime periodEnd, List<String> uris);
 }
