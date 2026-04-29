6e4f5c4d83c35665ea3dc4148ae8a5a4180d6353
Kamil Medjidov <captain-kam@yandex.ru>
2026-04-29 23:48:32 +0400

    Добавил конвертер для парсинга строк в LocalDateTime, для удобства работы с URL

--- ewm-main-service/src/main/java/ru/practicum/config/JacksonConfig.java
+++ ewm-main-service/src/main/java/ru/practicum/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package ru.practicum.validation;
+package ru.practicum.config;
 
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
